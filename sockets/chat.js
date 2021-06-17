eb0ccc68f58408004c11081a17ca2422b986ce3e
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-16 20:54:56 -0500

    adding chat.js

---
+++ sockets/chat.js
@@ -0,0 +1,10 @@
+module.exports = (io, socket) => {
+
+    // Listen for "new user" socket emits
+    socket.on('new user', (username) => {
+        console.log(`${username} has joined the chat!`);
+        io.emit('new user', username);
+    });
+
+
+}
