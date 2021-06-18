f212f5f3fa73807c39ed7dc906541cd2d78dc8ee
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-17 22:08:50 -0500

    updates for multiple channels support

--- public/index.js
+++ public/index.js
@@ -4,6 +4,13 @@ $(document).ready(()=>{
     let currentUser;
     // Get the online users from the server
     socket.emit('get online users');
+    socket.emit('user changed channel', 'General');
+
+    // changing channels by click
+    $(document).on('click', '.channel', (e) => {
+        let newChannel = e.target.textContent;
+        socket.emit('user changed channel', newChannel);
+    });
 
     $('#create-user-btn').click((e) => {
       e.preventDefault();
@@ -20,6 +27,8 @@ $(document).ready(()=>{
 
     $('#send-chat-btn').click((e) => {
         e.preventDefault();
+        // get current channel name
+        let channel = $('.channel-current').text();
         // get the message text value
         let message = $('#chat-input').val();
         // make sure message is not empty
@@ -27,9 +36,20 @@ $(document).ready(()=>{
             // emit the message with the current user to the server
             socket.emit('new message', {
                 sender: currentUser,
-                message: message
+                message: message,
+                channel: channel
             });
             $('#chat-input').val("");
+        }
+    });
+
+    $('#new-channel-btn').click(() => {
+        let newChannel = $('#new-channel-input').val();
+
+        if (newChannel.length > 0) {
+            // share new channel with the server
+            socket.emit('new channel', newChannel);
+            $('#new-channel-input').val("");
         }
     });
 
@@ -42,12 +62,16 @@ $(document).ready(()=>{
 
     //Output the new message
     socket.on('new message', (data) => {
-        $('.message-container').append(`
-        <div class="message">
-            <p class="message-user">${data.sender}: </p>
-            <p class="message-text">${data.message}</p>
-        </div>
-        `);
+        let currentChannel = $('.channel-current').text();
+        // only append the message if the curent user is currently in that channel
+        if(currentChannel == data.channel) {
+            $('.message-container').append(`
+            <div class="message">
+                <p class="message-user">${data.sender}: </p>
+                <p class="message-text">${data.message}</p>
+            </div>
+            `);
+        }
     });
 
     socket.on('get online users', (onlineUsers) => {
@@ -62,5 +86,23 @@ $(document).ready(()=>{
             $('.users-online').append(`<div class="user-online">${username}</div>`);
         }
     });
+
+    socket.on('user changed channel', (data) => {
+        $('.channel-current').addClass('channel');
+        $('.channel-current').removeClass('channel-current');
+        $(`.channel:contains('${data.channel}')`).addClass('channel-current');
+        $('.channel-current').removeClass('channel');
+        $('.message').remove();
+        data.messages.forEach((message) => {
+          $('.message-container').append(`
+            <div class="message">
+              <p class="message-user">${message.sender}: </p>
+              <p class="message-text">${message.message}</p>
+            </div>
+          `);
+        });
+    });
+
+
 
 });
