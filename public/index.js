106034c6926d0e7070a53cbcefba8cbe5674a182
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-18 20:06:46 -0500

    updates to enable creation of mulitple channels

--- public/index.js
+++ public/index.js
@@ -1,43 +1,43 @@
 $(document).ready(()=>{
     const socket = io.connect();
-
+  
+    //Keep track of the current user
     let currentUser;
-    // Get the online users from the server
+
+    // get all online users
     socket.emit('get online users');
-    socket.emit('user changed channel', 'General');
+    socket.emit('user changed channel', "General");
 
     // changing channels by click
     $(document).on('click', '.channel', (e) => {
         let newChannel = e.target.textContent;
-        socket.emit('user changed channel', newChannel);
+        socket.emit('user changed channel', newChannel)
     });
 
-    $('#create-user-btn').click((e) => {
+    $('#create-user-btn').click((e)=>{
       e.preventDefault();
-
-      let username = $('#username-input').val();
-      if(username.length > 0){
-        //Emit to the server the new user
-        socket.emit('new user', username);
-        currentUser = username;
+      if($('#username-input').val().length > 0){
+        socket.emit('new user', $('#username-input').val());
+        // Save the current user when created
+        currentUser = $('#username-input').val();
         $('.username-form').remove();
         $('.main-container').css('display', 'flex');
       }
     });
-
+  
     $('#send-chat-btn').click((e) => {
         e.preventDefault();
         // get current channel name
         let channel = $('.channel-current').text();
-        // get the message text value
+        // Get the message text value
         let message = $('#chat-input').val();
-        // make sure message is not empty
-        if(message.length > 0) {
-            // emit the message with the current user to the server
+        // Make sure it's not empty
+        if(message.length > 0){
+            // Emit the message with the current user to the server
             socket.emit('new message', {
-                sender: currentUser,
-                message: message,
-                channel: channel
+            sender : currentUser,
+            message : message,
+            channel: channel,
             });
             $('#chat-input').val("");
         }
@@ -45,25 +45,23 @@ $(document).ready(()=>{
 
     $('#new-channel-btn').click(() => {
         let newChannel = $('#new-channel-input').val();
-
-        if (newChannel.length > 0) {
+        console.log(newChannel)
+        if (newChannel.length > 0){
             // share new channel with the server
             socket.emit('new channel', newChannel);
             $('#new-channel-input').val("");
         }
     });
+  
+    //socket listeners
+    socket.on('new user', (username) => {
+      console.log(`${username} has joined the chat`);
+      $('.users-online').append(`<div class="user-online">${username}</div>`);
+    })
 
-      // Listen for "new user" socket emits
-      socket.on('new user', (username) => {
-        console.log(`${username} has joined the chat!`);
-        // Add the new user to the online user div
-        $('.users-online').append(`<div class="user-online">${username}</div>`);
-    });
-
-    //Output the new message
     socket.on('new message', (data) => {
         let currentChannel = $('.channel-current').text();
-        // only append the message if the curent user is currently in that channel
+        // only append the message if the user is currently in that channel
         if(currentChannel == data.channel) {
             $('.message-container').append(`
             <div class="message">
@@ -72,21 +70,27 @@ $(document).ready(()=>{
             </div>
             `);
         }
-    });
+    })
 
     socket.on('get online users', (onlineUsers) => {
-        for (username in onlineUsers) {
+        for (username in onlineUsers){
             $('.users-online').append(`<div class="user-online">${username}</div>`);
         }
-    });
+    })
 
     socket.on('user has left', (onlineUsers) => {
         $('.users-online').empty();
-        for (username in onlineUsers) {
+        for (username in onlineUsers){
             $('.users-online').append(`<div class="user-online">${username}</div>`);
         }
     });
 
+    socket.on('new channel', (newChannel) => {
+        $('.channels').append(`<div class="channel">${newChannel}</div>`);
+    });
+
+    // Make the channel joined the current channel. Then load the messages.
+    // This only fires for the client who made the channel.
     socket.on('user changed channel', (data) => {
         $('.channel-current').addClass('channel');
         $('.channel-current').removeClass('channel-current');
@@ -103,6 +107,4 @@ $(document).ready(()=>{
         });
     });
 
-
-
-});
+})
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,57 +1,52 @@
 module.exports = (io, socket, onlineUsers, channels) => {
 
-    // Listen for "new user" socket emits
     socket.on('new user', (username) => {
-        // Save the username as key to access the user's socket id
+        //Save the username as key to access the user's socket id
         onlineUsers[username] = socket.id;
-        // Saving the username to socket
-        socket['username'] = username;
-
+        //Save the username to socket as well. This is important for later.
+        socket["username"] = username;
         console.log(`✋ ${username} has joined the chat! ✋`);
-        io.emit('new user', username);
-    });
-
-     //Listen for new messages
+        io.emit("new user", username);
+    })
+  
+    //Listen for new messages
     socket.on('new message', (data) => {
-        // Send that data back to ALL clients
-        channels[data.channel].push({sender: data.sender, message: data.message});
-        io.to(data.channel).emit('new message', data);
-    });
+        console.log(data)
+      // Send that data back to ALL clients
+      channels[data.channel].push({sender: data.sender, message: data.message});
+      io.to(data.channel).emit('new message', data);
+    })
 
     socket.on('get online users', () => {
         socket.emit('get online users', onlineUsers);
-    });
+    })
 
     socket.on('disconnect', () => {
         delete onlineUsers[socket.username];
         io.emit('user has left', onlineUsers);
-    });
+    })
 
     socket.on('new channel', (newChannel) => {
-        // start new channel
+        //Save the new channel to our channels object. The array will hold the messages.
         channels[newChannel] = [];
-
-        // share info with socket
+        //Have the socket join the new channel room.
         socket.join(newChannel);
-        // inform all 
+        //Inform all clients of the new channel.
         io.emit('new channel', newChannel);
-
-        // share data to client
+        //Emit to the client that made the new channel, to change their channel to the one they made.
         socket.emit('user changed channel', {
-            channel: newChannel,
-            messages: channels[newChannel]
+          channel : newChannel,
+          messages : channels[newChannel]
         });
     });
 
     socket.on('user changed channel', (newChannel) => {
         socket.join(newChannel);
-
-        // share date to client
         socket.emit('user changed channel', {
-            channel: newChannel,
-            messages: channels[newChannel]
+          channel : newChannel,
+          messages : channels[newChannel]
         });
-        console.log('server saw chanel')
-    })
-
+      });
+    
+  
 }
